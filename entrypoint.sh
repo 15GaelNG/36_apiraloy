57b1b166e9c47303d00b9205d6bf5443975d0c79
Jesús Gael Núñez Gutiérrez <93097333+15GaelNG@users.noreply.github.com>
2023-10-09 12:10:58 -0600

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,11 @@
+#Obtener la IP del host en tiempo de ejecucion
+SERVER_IP=$(ip route get 1 | awk '{print $NF;exit}')
+# Mostrar la IP obtenida (puedes eliminar esto en producción)
+echo "La IP del host es: $SERVER_IP"
+
 echo "Run the container"
-python3 app.py
+
+export FLASK_RUN_HOST="$SERVER_IP"
+
+flask run
+
